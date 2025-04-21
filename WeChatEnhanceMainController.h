f1228b9e207452c887e1efd523bd15598b020ecb
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-21 13:39:05 +0800

    Update WeChatEnhanceMainController.h

--- WeChatEnhanceMainController.h
+++ WeChatEnhanceMainController.h
@@ -1,4 +1,7 @@
 // WeChatEnhanceMainController.h
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 @interface WeChatEnhanceMainController : UIViewController
 @property (nonatomic, strong) UITableView *tableView;
 @end
