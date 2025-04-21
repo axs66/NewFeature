2aac87bf95346bfb0c80084621d073dba7ad3ef3
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-21 13:33:41 +0800

    Create WeChatEnhanceMainController.m

---
+++ WeChatEnhanceMainController.m
@@ -0,0 +1,8 @@
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"微信增强";
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    // 添加你的功能列表...
+}
+
+@end
